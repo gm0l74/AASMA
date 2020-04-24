bb6290c163ff433c2390a62904ee86dc7d190be5
gm0l74 <gmoita.gm@gmail.com>
2020-04-24 21:15:40 +0100

    README updated

---
+++ env/environment.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.7
+#---------------------------------
+# AAMA - Environment
+# File : environment.py
+#
+# @ start date          23 03 2020
+# @ last update         23 03 2020
+#---------------------------------
+
+#---------------------------------
+# Imports
+#---------------------------------
+import pygame, time
+
+# Histologia#ANO, n cassete, tipo de coloracao, n cassete relativo ao caso
+# H<ANO>,<73245>,HB,1
+
+#---------------------------------
+# class Environment
+#---------------------------------
+class Environment:
+    def __init__(self):
+        pass
+
+    def __build(self):
+        pass
+
+    def __spawn(self):
+        pass
---
+++ env/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+#---------------------------------
+# AAMA - Environment
+# File : run.py
+#
+# @ start date          23 03 2020
+# @ last update         23 03 2020
+#---------------------------------
